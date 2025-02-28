f38ca85ff440f967dc55752986d0eb673a1ed2ad
tongwz <781425393@qq.com>
2025-02-28 13:32:58 +0800

    xiugai

--- config/launch.lua
+++ config/launch.lua
@@ -1,4 +1,5 @@
 local platform = require('utils.platform')()
+local wezterm = require 'wezterm'
 
 local options = {
     default_prog = {},
@@ -25,9 +26,39 @@ elseif platform.is_mac then
     options.launch_menu = {
         { label = 'Bash', args = { 'bash' } },
         { label = 'Fish', args = { '/opt/homebrew/bin/fish' } },
-        { label = '测试服_47.96.131.52', args = { 'ssh', 'root@47.96.131.52' } },
-        { label = 'dev_114.55.64.104', args = { 'ssh', 'root@114.55.64.104' } },
+        { label = 'testHost', args = { 'ssh', 'root@47.96.131.52' } },
+        { label = 'devHost', args = { 'ssh', 'root@114.55.64.104' } },
     }
 end
+
+-- 用于存储启动菜单项的映射表
+local label_map = {}
+for _, item in ipairs(options.launch_menu) do
+    if item.args and item.args[1] == 'ssh' then
+        local hostname = item.args[2]:match("([^@]+)$")
+        label_map[hostname] = item.label
+    end
+end
+
+-- 监听面板激活事件
+wezterm.on('pane-active', function(pane)
+    -- 获取当前面板所在的标签
+    local tab = pane:tab()
+    -- 获取当前终端的命令行
+    local cmd = pane:get_foreground_process_info().command_line
+    if cmd and cmd:match("^ssh") then
+        -- 提取主机名
+        local hostname = cmd:match("ssh%s+[^@]+@([^:%s]+)")
+        if hostname then
+            -- 使用启动菜单中的 label 作为标题
+            local label = label_map[hostname]
+            if label then
+                tab:set_title(label)
+            else
+                tab:set_title(hostname) -- 如果没有匹配的 label，使用主机名
+            end
+        end
+    end
+end)
 
 return options
