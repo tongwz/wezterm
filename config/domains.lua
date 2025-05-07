12530eb4ba83737577a86c520b855bedd55eb5f7
tongweizhen <tongweizhen@foxmail.com>
2025-05-07 20:14:17 +0800

    修改ssh连接的配置位置

--- config/domains.lua
+++ config/domains.lua
@@ -1,44 +1,66 @@
 return {
-   -- ref: https://wezfurlong.org/wezterm/config/lua/SshDomain.html
-   ssh_domains = {
-      {
-         -- The connection is an ssh connection, don't use any multiplexing.
-         multiplexing = 'None',
+    -- ref: https://wezfurlong.org/wezterm/config/lua/SshDomain.html
+    ssh_domains = {
+        {
+            multiplexing = 'None',
+            name = "DevNow0325",
+            remote_address = "101.37.29.111:22",
+            username = "root",
+            ssh_option = {},
+        },
+        {
+            -- The connection is an ssh connection, don't use any multiplexing.
+            multiplexing = 'None',
 
-         -- The name of this specific domain. Must be unique amongst
-         name = 'Kali-linux',
+            -- The name of this specific domain. Must be unique amongst
+            name = 'test1',
 
-         -- identifies the host:port pair of the remote server
-         -- Can be a DNS name or an IP address with an optional
-         -- ":port" on the end.
-         remote_address = '192.168.44.147:22',
+            -- identifies the host:port pair of the remote server
+            -- Can be a DNS name or an IP address with an optional
+            -- ":port" on the end.
+            remote_address = '120.26.121.166:22',
 
-         -- Whether agent auth should be disabled.
-         -- Set to true to disable it.
-         -- no_agent_auth = false,
+            -- Whether agent auth should be disabled.
+            -- Set to true to disable it.
+            -- no_agent_auth = false,
 
-         -- The username to use for authenticating with the remote host
-         username = 'kali',
+            -- The username to use for authenticating with the remote host
+            username = 'root',
 
-         -- Specify a SSH connection authentication file
-         -- Default authentication file is "~/.ssh/id_rsa"
-         ssh_option = {
-            identityfile = 'C:\\Users\\Fizz\\.ssh\\id_rsa',
-         },
-      },
-   },
+            -- Specify a SSH connection authentication file
+            -- Default authentication file is "~/.ssh/id_rsa"
+            ssh_option = {
+                -- identityfile = 'C:\\Users\\Fizz\\.ssh\\id_rsa',
+            },
+        },
+        {
+            multiplexing = 'None',
+            name = "test2",
+            remote_address = "120.26.121.89:22",
+            username = "root",
+            ssh_option = {},
+        },
+        {
+            multiplexing = 'None',
+            name = "TestSync",
+            remote_address = "114.55.64.104:22",
+            username = "root",
+            ssh_option = {},
+        },
+        {
+            multiplexing = 'None',
+            name = "rabbitmq",
+            remote_address = "47.111.137.185:22",
+            username = "root",
+            ssh_option = {},
+        },
+    },
 
-   -- ref: https://wezfurlong.org/wezterm/multiplexing.html#unix-domains
-   unix_domains = {},
+    -- ref: https://wezfurlong.org/wezterm/multiplexing.html#unix-domains
+    unix_domains = {},
 
-   -- ref: https://wezfurlong.org/wezterm/config/lua/WslDomain.html
-   wsl_domains = {
-      {
-         name = 'WSL:Ubuntu',
-         distribution = 'Ubuntu',
-         username = 'kevin',
-         default_cwd = '/home/kevin',
-         default_prog = { 'fish' },
-      },
-   },
+    -- ref: https://wezfurlong.org/wezterm/config/lua/WslDomain.html
+    wsl_domains = {
+        -- { name = 'WSL:Ubuntu', distribution = 'Ubuntu', username = 'kevin', default_cwd = '/home/kevin', default_prog = { 'fish' }, },
+    },
 }
--- config/launch.lua
+++ config/launch.lua
@@ -24,13 +24,13 @@ if platform.is_win then
 elseif platform.is_mac then
     options.default_prog = { '/opt/homebrew/bin/fish' }
     options.launch_menu = {
-        { label = 'Bash', args = { 'bash' } },
+        -- { label = 'Bash', args = { 'bash' } },
         { label = 'Fish', args = { '/opt/homebrew/bin/fish' } },
-        { label = 'testHost1', args = { 'ssh', 'root@120.26.121.166' } },
-        { label = 'testHost2', args = { 'ssh', 'root@120.26.121.89' } },
-        { label = 'testSync', args = { 'ssh', 'root@114.55.64.104' } },
-        { label = 'devNow0325', args = { 'ssh', 'root@101.37.29.111' } },
-        { label = 'rabbitmq', args = { 'ssh', 'root@47.111.137.185' } },
+        -- { label = 'TestHost1', args = {'env', 'WEZTERM_TITLE=TestHost1', 'ssh', 'root@120.26.121.166' } },
+        -- { label = 'TestHost2', args = { 'ssh', 'root@120.26.121.89' } },
+        -- { label = 'TestSync', args = { 'ssh', 'root@114.55.64.104' } },
+        -- { label = 'DevNow0325', args = { 'ssh', 'root@101.37.29.111' } },
+        -- { label = 'rabbitmq', args = { 'ssh', 'root@47.111.137.185' } },
     }
 end
 
@@ -43,10 +43,34 @@ for _, item in ipairs(options.launch_menu) do
     end
 end
 
+-- 监听面板焦点变化事件
+wezterm.on('pane-focus-changed', function(palette, pane)
+    -- 获取当前终端的命令行
+    local foreground_info = pane:get_foreground_process_info()
+    wezterm.log_info("myCustomFunction 被调用，testVar 的值为:", tab);
+    if foreground_info and foreground_info.command_line then
+        local cmd = foreground_info.command_line
+        if cmd:match("^ssh") then
+            -- 提取主机名
+            local hostname = cmd:match("ssh%s+[^@]+@([^:%s]+)")
+            if hostname then
+                -- 使用启动菜单中的 label 作为标题
+                local label = label_map[hostname]
+                if label then
+                    pane:tab():set_title(label)
+                else
+                    pane:tab():set_title(hostname) -- 如果没有匹配的 label，使用主机名
+                end
+                end
+        end
+    end
+end)
+
 -- 监听面板激活事件
 wezterm.on('pane-active', function(pane)
     -- 获取当前面板所在的标签
     local tab = pane:tab()
+    -- wezterm.log_info("myCustomFunction 被调用，testVar 的值为:", tab);
     -- 获取当前终端的命令行
     local cmd = pane:get_foreground_process_info().command_line
     if cmd and cmd:match("^ssh") then
